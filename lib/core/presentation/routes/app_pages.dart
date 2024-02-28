853223efc1a1be00331856e4738d8dd278d814d0
Ridha Ahmad Firdaus <ridhaaf@gmail.com>
2024-02-28 15:57:57 +0700

    Initial commit

---
+++ lib/core/presentation/routes/app_pages.dart
@@ -0,0 +1,8 @@
+import 'package:ridhaaf_flutter/core/presentation/routes/app_routes.dart';
+import 'package:ridhaaf_flutter/feature/presentation/pages/home/home_page.dart';
+
+class AppPages {
+  static final pages = {
+    AppRoutes.home: (context) => const HomePage(),
+  };
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:ridhaaf_flutter/core/presentation/routes/app_pages.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ridha ahmad firdaus',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      routes: AppPages.pages,
+    );
+  }
+}
