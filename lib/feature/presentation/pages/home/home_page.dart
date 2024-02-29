7ecf191851e0d6835bf531381c2fb895182bf47f
Ridha Ahmad Firdaus <ridhaaf@gmail.com>
2024-02-29 12:54:20 +0700

    refactor: create reusable widgets

--- lib/feature/presentation/pages/home/home_page.dart
+++ lib/feature/presentation/pages/home/home_page.dart
@@ -15,30 +15,29 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: AppBody(
         child: SingleChildScrollView(
-          child: Container(
-            margin: const EdgeInsets.all(AppConstants.defaultMargin),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                _imageSection(),
-                const DefaultSpacer(),
-                _introductionSection(),
-                const DefaultSpacer(),
-                _worksSection(),
-                const DefaultSpacer(),
-                _focusSection(),
-                const DefaultSpacer(),
-                _contactSection(),
-                const DefaultSpacer(size: 4),
-                _linkedinSection(),
-                const DefaultSpacer(size: 4),
-                _githubSection(),
-                const DefaultSpacer(size: 4),
-                _instagramSection(),
-                const DefaultSpacer(size: 4),
-                _xSection(),
-              ],
-            ),
+          padding: const EdgeInsets.all(AppConstants.defaultMargin),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _imageSection(),
+              const DefaultSpacer(),
+              _introductionSection(),
+              const DefaultSpacer(),
+              _worksSection(),
+              const DefaultSpacer(),
+              _focusSection(),
+              const DefaultSpacer(),
+              _contactSection('ridhaaf@gmail.com', 'contact'),
+              const DefaultSpacer(size: 4),
+              _linkSection('www.linkedin.com/in/ridhaaf', 'linkedin'),
+              const DefaultSpacer(size: 4),
+              _linkSection('github.com/ridhaaf', 'github'),
+              const DefaultSpacer(size: 4),
+              _linkSection(
+                  'www.instagram.com/portfolionya.ridhaaf', 'instagram'),
+              const DefaultSpacer(size: 4),
+              _linkSection('x.com/ridhaaf_', 'x'),
+            ],
           ),
         ),
       ),
@@ -54,212 +53,96 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _introductionSection() {
-    String text1 = 'ridha';
-    String text2 =
-        ' is a software engineer/mobile developer (android & flutter developer) known for crafting sleek and user-friendly mobile applications. Holding a Bachelor\'s degree in ';
-    String text3 = 'Informatics Engineering';
-    String text4 = ' from ';
-    String text5 = 'Universitas Pasundan';
-    String text6 =
-        ' and have approximately 2 years of experience in the field.';
-
     return RichText(
       text: TextSpan(
         style: GoogleFonts.fragmentMono(),
         children: [
-          TextSpan(
-            text: text1,
-            style: const TextStyle(
-              fontWeight: AppConstants.bold,
-            ),
+          _textSpan('ridha', bold: true),
+          _textSpan(
+              ' is a software engineer/mobile developer (android & flutter developer) known for crafting sleek and user-friendly mobile applications. Holding a Bachelor\'s degree in '),
+          _linkSpan(
+            'https://if.unpas.ac.id',
+            'Informatics Engineering',
+            bold: true,
           ),
-          TextSpan(
-            text: text2,
+          _textSpan(' from '),
+          _linkSpan(
+            'https://www.unpas.ac.id',
+            'Universitas Pasundan',
+            bold: true,
           ),
-          TextSpan(
-            text: text3,
-            style: const TextStyle(
-              fontWeight: AppConstants.bold,
-              decoration: TextDecoration.underline,
-            ),
-            recognizer: TapGestureRecognizer()
-              ..onTap = () {
-                final Uri informaticsLaunchUri = Uri(
-                  scheme: 'https',
-                  path: 'if.unpas.ac.id',
-                );
-
-                launchUrl(informaticsLaunchUri);
-              },
-          ),
-          TextSpan(
-            text: text4,
-          ),
-          TextSpan(
-            text: text5,
-            style: const TextStyle(
-              fontWeight: AppConstants.bold,
-              decoration: TextDecoration.underline,
-            ),
-            recognizer: TapGestureRecognizer()
-              ..onTap = () {
-                final Uri unpasLaunchUri = Uri(
-                  scheme: 'https',
-                  path: 'www.unpas.ac.id',
-                );
-
-                launchUrl(unpasLaunchUri);
-              },
-          ),
-          TextSpan(
-            text: text6,
-          ),
+          _textSpan(
+              ' and have approximately 2 years of experience in the field.'),
         ],
       ),
     );
   }
 
   Widget _worksSection() {
-    String text1 = 'You can check out some of ';
-    String text2 = 'ridha';
-    String text3 = '\'s work on the ';
-    String text4 = 'projects';
-    String text5 = ' page.';
-
     return RichText(
       text: TextSpan(
         style: GoogleFonts.fragmentMono(),
         children: [
-          TextSpan(
-            text: text1,
-          ),
-          TextSpan(
-            text: text2,
-            style: const TextStyle(
-              fontWeight: AppConstants.bold,
-            ),
-          ),
-          TextSpan(
-            text: text3,
-          ),
-          TextSpan(
-            text: text4,
-            style: const TextStyle(
-              decoration: TextDecoration.underline,
-            ),
-            recognizer: TapGestureRecognizer()
-              ..onTap = () {
-                print('URL tapped');
-              },
-          ),
-          TextSpan(
-            text: text5,
-          ),
+          _textSpan('You can check out some of '),
+          _textSpan('ridha', bold: true),
+          _textSpan('\'s work on the '),
+          _linkSpan('#', 'projects', bold: false, onTap: () {}),
+          _textSpan(' page.'),
         ],
       ),
     );
   }
 
   Widget _focusSection() {
-    String text1 = 'ridha';
-    String text2 =
-        ' is currently focusing on mobile development, especially in the field of android and flutter.';
-
     return RichText(
       text: TextSpan(
         style: GoogleFonts.jetBrainsMono(),
         children: [
-          TextSpan(
-            text: text1,
-            style: const TextStyle(
-              fontWeight: AppConstants.bold,
-            ),
-          ),
-          TextSpan(
-            text: text2,
-          ),
+          _textSpan('ridha', bold: true),
+          _textSpan(
+              ' is currently focusing on mobile development, especially in the field of android and flutter.'),
         ],
       ),
     );
   }
 
-  Widget _contactSection() {
-    String text = 'contact';
-
+  Widget _contactSection(String email, String text) {
     return AppLinkText(
-      onTap: () {
-        final Uri emailLaunchUri = Uri(
-          scheme: 'mailto',
-          path: 'ridhaaf@gmail.com',
-        );
-
-        launchUrl(emailLaunchUri);
-      },
+      onTap: () => launchUrl(Uri(scheme: 'mailto', path: email)),
       text: text,
     );
   }
 
-  Widget _linkedinSection() {
-    String text = 'linkedin';
-
+  Widget _linkSection(String path, String text) {
     return AppLinkText(
-      onTap: () {
-        final Uri linkedinLaunchUri = Uri(
-          scheme: 'https',
-          path: 'www.linkedin.com/in/ridhaaf',
-        );
-
-        launchUrl(linkedinLaunchUri);
-      },
+      onTap: () => launchUrl(Uri(scheme: 'https', path: path)),
       text: text,
     );
   }
 
-  Widget _githubSection() {
-    String text = 'github';
-
-    return AppLinkText(
-      onTap: () {
-        final Uri githubLaunchUri = Uri(
-          scheme: 'https',
-          path: 'github.com/ridhaaf',
-        );
-
-        launchUrl(githubLaunchUri);
-      },
+  TextSpan _textSpan(String text, {bool bold = false}) {
+    return TextSpan(
       text: text,
+      style: TextStyle(
+        fontWeight: bold ? AppConstants.bold : AppConstants.regular,
+      ),
     );
   }
 
-  Widget _instagramSection() {
-    String text = 'instagram';
-
-    return AppLinkText(
-      onTap: () {
-        final Uri instagramLaunchUri = Uri(
-          scheme: 'https',
-          path: 'www.instagram.com/portfolionya.ridhaaf',
-        );
-
-        launchUrl(instagramLaunchUri);
-      },
+  TextSpan _linkSpan(
+    String url,
+    String text, {
+    bool bold = false,
+    Function()? onTap,
+  }) {
+    return TextSpan(
       text: text,
-    );
-  }
-
-  Widget _xSection() {
-    String text = 'x';
-
-    return AppLinkText(
-      onTap: () {
-        final Uri xLaunchUri = Uri(
-          scheme: 'https',
-          path: 'x.com/ridhaaf_',
-        );
-
-        launchUrl(xLaunchUri);
-      },
-      text: text,
+      style: TextStyle(
+        fontWeight: bold ? AppConstants.bold : AppConstants.regular,
+        decoration: TextDecoration.underline,
+      ),
+      recognizer: TapGestureRecognizer()
+        ..onTap = onTap ?? () => canLaunchUrl(Uri.parse(url)),
     );
   }
 }
